5a239cae40db1b5a7250ee837f41cac12ec76e81
kimhayong0 <wlfhdn@hanmail.net>
2021-07-26 12:53:01 +0900

    day02

---
+++ src/day02_variable/Ex02.java
@@ -0,0 +1,12 @@
+package day02_variable;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		int age = 25;
+		double weight = 50.0;
+		double height = 163.5;
+		System.out.println("나의나이는 "+age);
+		System.out.println("나의몸뭄게는 "+weight);
+		System.out.println("나의 키는 "+height);
+	}
+}
