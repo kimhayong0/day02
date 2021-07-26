4dc68822843b206bcf2df27dd3fa1af91e53a705
kimhayong0 <wlfhdn@hanmail.net>
2021-07-26 14:50:02 +0900

    day02 완료

---
+++ src/day02_variable/Ex07.java
@@ -0,0 +1,22 @@
+package day02_variable;
+
+import java.util.Scanner;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int age;
+		String name;
+		System.out.println("이름 입력: ");
+		name = scan.next();
+		System.out.println("나이 입력: ");
+		age = scan.nextInt();
+		System.out.println("이름: "+name);
+		System.out.println("나이: "+age);
+		
+		
+
+	}
+
+}
